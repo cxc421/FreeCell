2f181f98baf1f4608a4387d8277aca84c4b22a63
Chris Chuang <chrischuang0421@gmail.com>
2019-07-29 02:26:13 +0800

    Updates

---
+++ precache-manifest.1ce74cc1111cbbe79c1818b6254afb12.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17be3ca42e3e0748caf5a1e50a4ff90e",
+    "url": "/FreeCell/index.html"
+  },
+  {
+    "revision": "595818290a839e45a2e6",
+    "url": "/FreeCell/static/css/main.ffdd441f.chunk.css"
+  },
+  {
+    "revision": "2728fa00a295ec7c6bb7",
+    "url": "/FreeCell/static/js/2.5c4a30c5.chunk.js"
+  },
+  {
+    "revision": "595818290a839e45a2e6",
+    "url": "/FreeCell/static/js/main.9c51a139.chunk.js"
+  },
+  {
+    "revision": "acbe79be84bdd005ae5b",
+    "url": "/FreeCell/static/js/runtime~main.8ae443eb.js"
+  },
+  {
+    "revision": "c624ae4b6770c3e2cd6a485044e4651a",
+    "url": "/FreeCell/static/media/bg-img-main.c624ae4b.svg"
+  },
+  {
+    "revision": "47f7e23ce12d1ffc29ce533fd0c789bb",
+    "url": "/FreeCell/static/media/bg-img-wellcome.47f7e23c.svg"
+  },
+  {
+    "revision": "5a9d3fcb91f0fe429e338fe618e0d159",
+    "url": "/FreeCell/static/media/icon-club.5a9d3fcb.svg"
+  },
+  {
+    "revision": "529ba3652d38e03b5d16ec373da3f285",
+    "url": "/FreeCell/static/media/icon-diamond.529ba365.svg"
+  },
+  {
+    "revision": "b2863172c5dd1ec92f81d3a36e1a9d5b",
+    "url": "/FreeCell/static/media/icon-heart.b2863172.svg"
+  },
+  {
+    "revision": "b2f85c18e511aff4219af928270dbd89",
+    "url": "/FreeCell/static/media/icon-pause.b2f85c18.svg"
+  },
+  {
+    "revision": "59c7323891ece127023c013b1579e1a4",
+    "url": "/FreeCell/static/media/icon-prev-step.59c73238.svg"
+  },
+  {
+    "revision": "d967c2571c2ab3c79492bd02c31bc2bc",
+    "url": "/FreeCell/static/media/icon-question.d967c257.svg"
+  },
+  {
+    "revision": "e3794bf85c1dab35ace9bf011cdabc1e",
+    "url": "/FreeCell/static/media/icon-replay.e3794bf8.svg"
+  },
+  {
+    "revision": "fcd5e7804292a6f84cd2e3e346f2c692",
+    "url": "/FreeCell/static/media/icon-spades.fcd5e780.svg"
+  },
+  {
+    "revision": "a9f0452a0130634f7acfeebad1541f1e",
+    "url": "/FreeCell/static/media/logo.a9f0452a.svg"
+  },
+  {
+    "revision": "c29189fe72db6d3995ac71fab9813522",
+    "url": "/FreeCell/static/media/pic-card-elf.c29189fe.svg"
+  },
+  {
+    "revision": "d84891375d087b1113716cd41a09c078",
+    "url": "/FreeCell/static/media/pic-card-king.d8489137.svg"
+  },
+  {
+    "revision": "92a4da432c4c031b28874bb2605a4f8c",
+    "url": "/FreeCell/static/media/pic-card-knight.92a4da43.svg"
+  }
+]);
